cfc2662fc42e42f35ed122c676d7c6a0c3abf5a4
Fernando Nicolás <he440185@uaeh.edu.mx>
2023-09-17 15:29:31 -0600

    Mapeo de la Entidad #2

--- src/main/java/com/latam/alura/tienda/modelo/Producto.java
+++ src/main/java/com/latam/alura/tienda/modelo/Producto.java
@@ -1,4 +1,50 @@
-package com.latam.alura.tienda.model;
+package com.latam.alura.tienda.modelo;
 
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "productos")
 public class Producto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nombre;
+    private String descripcion;
+    private BigDecimal precio;
+
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public BigDecimal getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(BigDecimal precio) {
+        this.precio = precio;
+    }
 }
